6b4da3b89abd2f02367dabb3d5260e45fe6572bd
iamjerriz <56684439+iamjerriz@users.noreply.github.com>
2021-01-06 12:15:44 +0800

    Add files via upload

---
+++ build/precache-manifest.cf006f005b53e75f25f2c5db2711334b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f867bef91840a4c084f696800127d9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87f845e3aa59718b00f7",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "f333e1477bd7200aa643",
+    "url": "/static/css/main.c32687cd.chunk.css"
+  },
+  {
+    "revision": "87f845e3aa59718b00f7",
+    "url": "/static/js/2.dcc5a5af.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.dcc5a5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f333e1477bd7200aa643",
+    "url": "/static/js/main.f9f2d87f.chunk.js"
+  },
+  {
+    "revision": "1cef7fc43aae665638ba",
+    "url": "/static/js/runtime-main.f9a152df.js"
+  }
+]);
